6928497ebe2e4479fc5c74a0d6c9d36a830c6f44
kcshrad <kcshrad@gmail.com>
2021-12-19 18:10:45 +1100

    updated weather forecast for current location

--- src/index.js
+++ src/index.js
@@ -134,11 +134,6 @@ function search(searchInput) {
 function searchCurrent(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city").value;
-  //let cityInput = document.querySelector("#current-city");
-  //cityInput.innerHTML = `${searchInput.value}`;
-  //let apiKey = "409c92218249e96f0e8de22a43f0bc4b";
-  // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}&units=metric`;
-
   search(searchInput);
 }
 function showFahrenheit(event) {
@@ -164,39 +159,19 @@ fahrenheitButton.addEventListener("click", showFahrenheit);
 let celsiusButton = document.querySelector(".celsius");
 celsiusButton.addEventListener("click", showCelsius);
 
-search("New York");
+let button = document.querySelector("#current-location");
+button.addEventListener("click", getCurrentLocationTemperature);
 
-function currentLocationTemperature(response) {
-  //let temperature = Math.round(response.data.main.temp);
-  let button = document.querySelector("#current-location");
-  button.addEventListener("click", getCurrentLocationTemperature);
-
-  function getCurrentLocationTemperature(event) {
-    event.preventDefault();
-    let city = response.data.name;
-    let newCurrentTemp = document.querySelector(".currentTemp");
-    newCurrentTemp.innerHTML = `${Math.round(response.data.main.temp)}°C`;
-    let h2 = document.querySelector("#current-city");
-    h2.innerHTML = city;
-
-    let weatherType = response.data.weather[0].main;
-    let newweather = document.querySelector(".description");
-    newweather.innerHTML = weatherType;
-
-    let humidity = document.querySelector("#humidity");
-    let newHumidity = response.data.main.humidity;
-    humidity.innerHTML = `Humidity ${newHumidity}%`;
-
-    let pressure = document.querySelector("#pressure");
-    let newPressure = response.data.main.pressure;
-    pressure.innerHTML = ` Pressure ${newPressure}`;
-  }
+function getCurrentLocationTemperature(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
 }
+
 function showLocation(position) {
   let apiKey = "409c92218249e96f0e8de22a43f0bc4b";
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(currentLocationTemperature);
+  axios.get(apiUrl).then(showTemperature);
 }
-navigator.geolocation.getCurrentPosition(showLocation);
+search("New York");
